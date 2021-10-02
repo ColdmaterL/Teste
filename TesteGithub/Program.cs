cbb9496a71e20d883aa3bbd2fb2fbc815d2d0733
ColdmaterL <lucasmenmor@hotmail.com>
2021-10-01 23:31:38 -0300

    Acrescentado Ola mundo em japones.

--- TesteGithub/Program.cs
+++ TesteGithub/Program.cs
@@ -4,6 +4,8 @@ namespace TesteGithub {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundo!");
+            Console.WriteLine("こんにちは世界!");
         }
     }
 }
